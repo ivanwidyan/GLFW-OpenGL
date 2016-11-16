6dab309441c179276c6699ce603b8e86977c8d27
Ivan Widyan <ivanwidyan@yahoo.com>
2016-11-16 22:02:18 +0100

    Drawing a Triangle

--- GLFWOpenGL/main.cpp
+++ GLFWOpenGL/main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
-
+// GLEW
 #define GLEW_STATIC
-
 #include <GL/glew.h>
+// GLFW
 #include <GLFW/glfw3.h>
-
+// window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
+const GLchar *vertexShaderSource = "#version 330 core\n"
+"layout ( location = 0) in vec3 position;\n"
+"void main( )\n"
+"{\n"
+"gl_Position = vec4( position.x, position.y, position.z, 1.0 );"
+"}";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main ( )\n"
+"{\n"
+"color = vec4( 0.0f, 0.5f, 0.9f, 1.0f );\n"
+"}";
+
 int main() {
+	// init GLFW
 	glfwInit();
+	// Set all the required options for GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+	// Create a GLFWindow object that we can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
 
 	int screenWidth, screenHeight;
@@ -28,25 +45,98 @@ int main() {
 	}
 
 	glfwMakeContextCurrent(window);
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
-
+	// Initialize GLEW to setup the OpenGL Function pointers
 	if (GLEW_OK != glewInit()) {
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return EXIT_FAILURE;
 	}
 
+	// Define the viewport dimensions
 	glViewport(0, 0, screenWidth, screenHeight);
 
+	// Vertex Shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	GLint success;
+	GLchar infolog[512];
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infolog << std::endl;
+	}
+
+	// Fragment Shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infolog);
+		std::cout << "ERROR::FRAGMENT::FRAGMENT::COMPILATION_FAILED\n" << infolog << std::endl;
+	}
+
+	// Shader Program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infolog);
+		std::cout << "ERROR::FRAGMENT::PROGRAM::LINKING_FAILED\n" << infolog << std::endl;
+	}
+
+	// Deleting vertex and fragment shader
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f, // bottom left
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid *) 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// Game loop
 	while (!glfwWindowShouldClose(window)) {
+		// Check if any events have been activated (key pressed, mouse move etc.) and call corresponding response function
 		glfwPollEvents();
+		// Render
+		// Clear the colorbuffer
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// draw opengl stuff
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
+		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	// Terminate GLFW, clearing any resources allocated by GLFW
 	glfwTerminate();
 	return EXIT_SUCCESS;
 }
