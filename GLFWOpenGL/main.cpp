2f8eded5f7afa7a7238fa433465a3298cb1d25d3
Ivan Widyan <ivanwidyan@yahoo.com>
2016-11-16 20:48:12 +0100

    Setup GLEW and GLFW on Visual Studio

---
+++ GLFWOpenGL/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#define GLEW_STATIC
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
+
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+	glewExperimental = GL_TRUE;
+
+	if (GLEW_OK != glewInit()) {
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	glViewport(0, 0, screenWidth, screenHeight);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw opengl stuff
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
