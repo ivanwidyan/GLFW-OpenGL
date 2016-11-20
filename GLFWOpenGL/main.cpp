c5090cf68f2f972a0e128556cf4bfc6dd7193330
Ivan Widyan <ivanwidyan@yahoo.com>
2016-11-20 20:44:25 +0100

    Triangle and Rectangle

--- GLFWOpenGL/main.cpp
+++ GLFWOpenGL/main.cpp
@@ -4,235 +4,174 @@
 #include <GL/glew.h>
 // GLFW
 #include <GLFW/glfw3.h>
-// SOIL2
-#include "SOIL2/SOIL2.h"
-// GLM
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"}\n\0";
 
-// Other includes
-#include "Shader.h"
-
-// window dimensions
-const GLint WIDTH = 800, HEIGHT = 600;
-
-int main() {
-	// init GLFW
+int main()
+{
+	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+	// Init GLFW
 	glfwInit();
 	// Set all the required options for GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	// Create a GLFWindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
-
-	int screenWidth, screenHeight;
-	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
-
-	if (window == nullptr)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return EXIT_FAILURE;
-	}
-
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+
 	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
+
 	// Initialize GLEW to setup the OpenGL Function pointers
-	if (GLEW_OK != glewInit()) {
-		std::cout << "Failed to initialize GLEW" << std::endl;
-		return EXIT_FAILURE;
-	}
+	glewInit();
 
 	// Define the viewport dimensions
-	glViewport(0, 0, screenWidth, screenHeight);
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
 
-	glEnable(GL_DEPTH_TEST);
+	// Build and compile our shader program
+	// Vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
 
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	// Check for compile time errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 
-	// Build and compile my shader program
-	Shader ourShader("res/shaders/core.vert", "res/shaders/core.frag");
+	// Fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Check for compile time errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Link shaders
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	// Set up vertex data (and buffer(s)) and attribute pointers
-	// Use with Orthographic projection
-	/*GLfloat vertices[] = {
-	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,
-	0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 0.0f,
-	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-	-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,
-
-	-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-	0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,
-	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,
-	-0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 1.0f,
-	-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-
-	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-	-0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-	-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-
-	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-	0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-	0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-	0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-
-	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-	0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-	0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-	0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-	-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-
-	-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
-	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
-	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
-	-0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
-	-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f
-	};*/
-	
-	// use with perspective projection
+	//GLfloat vertices[] = {
+	//  // First triangle
+	//   0.5f,  0.5f,  // Top Right
+	//   0.5f, -0.5f,  // Bottom Right
+	//  -0.5f,  0.5f,  // Top Left 
+	//  // Second triangle
+	//   0.5f, -0.5f,  // Bottom Right
+	//  -0.5f, -0.5f,  // Bottom Left
+	//  -0.5f,  0.5f   // Top Left
+	//}; 
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		0.5f,  0.5f, 0.0f,  // Top Right
+		0.5f, -0.5f, 0.0f,  // Bottom Right
+		-0.5f, -0.5f, 0.0f,  // Bottom Left
+		-0.5f,  0.5f, 0.0f   // Top Left 
+	};
+	GLuint indices[] = {  // Note that we start from 0!
+		0, 1, 3,  // First Triangle
+		1, 2, 3   // Second Triangle
 	};
 
-	GLuint VBO, VAO;
+	GLuint VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-
-	// Bind the Vertex Array Object first, then bind and vertex buffer(s) and attribute pointer(s)
+	glGenBuffers(1, &EBO);
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
 	glBindVertexArray(VAO);
-	
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid *) 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	// Texture coordinate attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid *)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(2);
 
-	glBindVertexArray(0); // Unbind VAO
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
 
-	GLuint texture;
-	int width, height;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	unsigned char *image = SOIL_load_image("res/images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	glm::mat4 projection;
-	projection = glm::perspective(45.0f, (GLfloat) screenWidth / (GLfloat) screenHeight, 0.1f, 1000.0f);
-	//projection = glm::ortho(0.0f, (GLfloat)screenWidth, 0.0f, (GLfloat)screenHeight, 0.1f, 1000.f);
+	// Uncommenting this call will result in wireframe polygons.
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// Game loop
-	while (!glfwWindowShouldClose(window)) {
-		// Check if any events have been activated (key pressed, mouse move etc.) and call corresponding response function
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
+
 		// Render
 		// Clear the colorbuffer
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.0f, 0.5f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture"), 0);
-
-		ourShader.Use();
-
-		glm::mat4 model;
-		glm::mat4 view;
-		model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.f, 0.f));
-		view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
-		//model = glm::rotate(model, 0.5f, glm::vec3(1.f, 0.f, 0.f));
-		//view = glm::translate(view, glm::vec3(screenWidth / 2, screenHeight / 2, -700.f));
-
-		GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
-		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
-		GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
-		
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
+		// Draw our first triangle
+		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
-
 	// Properly de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-
-	// Terminate GLFW, clearing any resources allocated by GLFW
+	glDeleteBuffers(1, &EBO);
+	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
-	return EXIT_SUCCESS;
+	return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
